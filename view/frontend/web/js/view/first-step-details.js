1ba1a40520e5113e5dca620a422c6df56f7cba5e
Nam Luu <nam.luuduc@gmail.com>
2022-12-16 23:56:49 +0700

    sync address data and reload shipping method when go to step 2

--- view/frontend/web/js/view/first-step-details.js
+++ view/frontend/web/js/view/first-step-details.js
@@ -1,12 +1,20 @@
 define([
+    'jquery',
     'ko',
     'uiComponent',
     'underscore',
     'Magento_Checkout/js/model/step-navigator',
-    'uiRegistry'
-], function(ko, Component, _, stepNavigator, registry) {
+    'uiRegistry',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/shipping-rate-registry',
+    'Magento_Checkout/js/model/checkout-data-resolver',
+    'Magento_Checkout/js/checkout-data',
+    'Magento_Checkout/js/model/shipping-rates-validator',
+    'Magento_Customer/js/model/customer'
+], function($, ko, Component, _, stepNavigator, registry, quote, rateRegistry, 
+            checkoutDataResolver, checkoutData, shippingRatesValidator, customer) {
     'use strict';
-    
+
     return Component.extend({
         defaults: {
             sortOrder: 10,
@@ -15,14 +23,18 @@ define([
             fieldComponentsOfThisStep: [
                 "checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset.firstname",
                 "checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset.lastname",
-                "checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset.telephone"
-            ]
+                "checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset.telephone",
+                "checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset.postcode"
+            ],
+            loginFormSelector: 'form[data-role=email-with-possible-login]'
         },
-        
+
         isVisible: ko.observable(true),
-        
+
         initialize: function () {
             this._super();
+            var fieldsetName = 'checkout.steps.customer-details-step.customer-details-area.customer-details-fieldset';
+
             stepNavigator.registerStep(
                 this.stepCode,
                 null,
@@ -31,17 +43,48 @@ define([
                 _.bind(this.navigate, this),
                 this.sortOrder
             );
+
+            // sync the data checkoutData local storage
+            registry.async('checkoutProvider')(function (checkoutProvider) {
+                var shippingAddressData = checkoutData.getShippingAddressFromData();
+
+                if (shippingAddressData) {
+                    checkoutProvider.set(
+                        'shippingAddress',
+                        $.extend(true, {}, checkoutProvider.get('shippingAddress'), shippingAddressData)
+                    );
+                }
+                checkoutProvider.on('shippingAddress', function (shippingAddrsData, changes) {
+                    var isPostcodeNotEmpty = shippingAddrsData.postcode && !_.isEmpty(shippingAddrsData.postcode);
+
+                    if (isPostcodeNotEmpty) {
+                        checkoutData.setShippingAddressFromData(shippingAddrsData);
+                    }
+                });
+                shippingRatesValidator.initFields(fieldsetName);
+            });
+
             return this;
         },
-        
+
         navigate: function () {
             this.isVisible(true);
         },
-        
+
         navigateToNextStep: function () {
-            if (!this.hasInvalidFields()) stepNavigator.next();
+            if (!this.hasInvalidFields() && this.isCustomerEmailValid()) {
+                /*var address = quote.shippingAddress();
+                address.trigger_reload = new Date().getTime();
+                rateRegistry.set(address.getKey(), null);
+                rateRegistry.set(address.getCacheKey(), null);
+                quote.shippingAddress(address);*/
+
+                // reload shipping method
+                checkoutDataResolver.resolveShippingAddress();
+                stepNavigator.next();
+            }
         },
-        
+
         hasInvalidFields: function () {
             var result = false;
             _.each(this.fieldComponentsOfThisStep, function ($nextElementName) {
@@ -55,6 +98,17 @@ define([
             });
 
             return result;
-        }
+        },
+
+        isCustomerEmailValid: function () {
+            var emailValidationResult = customer.isLoggedIn();
+
+            if (!customer.isLoggedIn()) {
+                $(this.loginFormSelector).validation();
+                emailValidationResult = Boolean($(this.loginFormSelector + ' input[name=username]').valid());
+            }
+
+            return emailValidationResult;
+        },
     });
 });
